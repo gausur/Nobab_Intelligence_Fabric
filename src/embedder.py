864831963f985a973f35f698328710884e1d380f
gausur <97968199+gausur@users.noreply.github.com>
2026-05-12 13:24:16 +0600

    Update embedder.py

--- src/embedder.py
+++ src/embedder.py
@@ -1,13 +1,12 @@
 import chromadb
 from sentence_transformers import SentenceTransformer
-from config import CHROMA_DIR, COLLECTION_NAME
+from src.config import CHROMA_DIR, COLLECTION_NAME
 
 embedder = SentenceTransformer("all-MiniLM-L6-v2")
 client = chromadb.PersistentClient(path=CHROMA_DIR)
 collection = client.get_or_create_collection(COLLECTION_NAME)
 
 def add_to_vectorstore(doc_id, text, metadata):
-    """ভেক্টর ডাটাবেসে ডকুমেন্ট যোগ করা"""
     embedding = embedder.encode([text]).tolist()[0]
     collection.upsert(
         ids=[doc_id],
